a0977415ae3e2797a7bbbea97e7bc2ea77896049
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-05-23 15:15:11 -0300

    Finalização de Eventos Lógicos

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,16 +1,13 @@
 function tocaSom(seletorAudio) {
     const elemento = document.querySelector(seletorAudio);
 
-    if (elemento === null) {
-        console.log('Elemento não encontrado');
-    }
-
-    if (elemento != null) {
-        if(elemento.localName === 'audio'){
+    if (elemento != null && elemento.localName === 'audio') {
             elemento.play();
-        }
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
     }  
-}
 
 //document.querySelector('.tecla_pom').onclick = tocaSomPom; selecionar somente um elemento
 
