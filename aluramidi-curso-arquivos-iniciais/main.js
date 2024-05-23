0473b2fb5fbef9d82c54375d882d3808e49342b5
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-05-23 13:39:31 -0300

    Utilizando String dinâmicas, e montando a lógica While

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,10 +1,29 @@
-// SomPOM-----------------------------
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 //document.querySelector('.tecla_pom').onclick = tocaSomPom; selecionar somente um elemento
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom 
+let contador = 0;
+
+//lista de repetição (enquanto)
+while (contador < listaDeTeclas.length) {
+
+    const tecla  = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    //template string
+    const idAudio = `#som_${instrumento}`;
+   
+   console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+
+    //console.log(contador);
+}
